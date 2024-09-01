a45f40531ce86126659ec6c76bf71a10de81d0b1
Dhirendra Thakur <drthunk@gmail.com>
2024-09-01 20:08:59 +0545

    dataabase table error has been resolved

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
 
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -10,19 +11,23 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
+    __tablename__ = 'todo'
     sno = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String, nullable=False)
     desc= db.Column(db.String(500), nullable=False)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
    ## data you want to see in table we use repr  
     def __repr__(self) -> str:
-        return f"{self.sno} -  {self.username}"
+        return f"{self.sno} -  {self.title}"
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/main_page')
 def main_page():
     todo = Todo(title ="First todo", desc = "Make an investment in crypto")
     db.session.add(todo)
-    db.session.commit()
+    db.session.commit() 
     alltodo = Todo.query.all()
     
     return render_template('index.html', alltodo=alltodo)
